66b80cec1ee254c172de588cf57a9c69a9540ed6
kupfermuetze <stefan.eisenhuth@gmail.com>
2023-09-28 18:23:09 +0200

    Exception handling added

--- tasmota2venus-mqtt_launcher.sh
+++ tasmota2venus-mqtt_launcher.sh
@@ -3,6 +3,11 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 
 cd /
-cd home/pi/tasmota2venus-mqtt
+cd home/root/tasmota2venus-mqtt
 python tasmota2venus-mqtt.py
 cd /
+
+
+
+@reboot sh /home/root/tasmota2venus-mqtt/tasmota2venus-mqtt_launcher.sh >/home/root/tasmota2venus-mqtt/logs/cronlog 2>&1
+
